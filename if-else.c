f917c5eb07cc172ee7fdb8ac25557a291a0ddd54
saiswp70-glitch <saiswp4396@gmail.com>
2026-09-07 20:38:58 +0530

    Add files via upload

---
+++ if-else.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main() 
+{
+    int n;
+    printf("enter number n");
+    scanf("%d",&n);
+
+    if(n%2==0)
+    {
+        printf("number is even");
+    }
+    else
+    {
+        printf("number is odd");
+    }
+    getch();
+    return 0;
+
+
+
+    }
