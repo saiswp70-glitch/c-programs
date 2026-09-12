99fe94b9dbbac84a8cd831acd9bdaa4913a1ff0d
saiswp70-glitch <saiswp4396@gmail.com>
2026-09-12 19:20:01 +0530

    Add files via upload

---
+++ Resume.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("--------------------------------------------------\n");
+    printf( "                      Resume\n");
+    printf("--------------------------------------------------\n");
+    printf("Name        :                   Soal Reaper\n");
+    printf("Phone No  :               8263027431\n");
+    printf("Email ID    :                sswp4396@gmail.com\n");
+    printf("City        :               Chhatrapatisambhajinagar\n");
+
+    printf("=============Education============\n");
+    printf("College      :                  Deogiri College\n");
+    printf("Branch       :                   Data science\n");
+    printf("CGPA         :                    9.7\n");
+
+    printf("============Skills==================\n");
+    printf("Python language\n");
+    printf("statastics\n");
+    printf("MYSQL\n");
+
+
+    printf("=================PROJECTS==========\n");
+    printf("IPL Data analysis using  real time data\n");
+    printf("Made website of name of Campus connect\n");
+
+    printf("=============WORKEXPERINCE=========\n");
+    printf("completed Data analytics intership for three months");
+    printf("made real world projects with my peers");
+
+    printf("===========CAREER ASCPECTS=========\n");
+    printf("work harder at any cost and go to the better place than todays standing\n");
+    
+    
+      
+    return 0;
+}
